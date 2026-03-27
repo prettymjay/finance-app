0a2d2b11240793ef95565bf86444be9d7701e8d9
prettymjay <tagaloguinmaereyjane@gmail.com>
2026-03-27 12:37:50 +0800

    first commit

---
+++ database/migrations/2026_03_27_042140_create_transactions_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('category_id');
+            $table->foreign('category_id')
+                ->references('id')
+                ->on('categories');
+            $table->string('reference_no')->nullable();
+            $table->string('transaction_type')->nullable();
+            $table->text('description')->nullable();
+            $table->double('amount')->default(0);
+            $table->dateTime('transaction_date')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('transactions');
+    }
+};
